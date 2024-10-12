5e92bcc5f3d115b838836ae82b26ab1d1fd91710
Henrik Nordlie - Nevion <henriknordlie99@gmail.com>
2024-10-12 12:26:10 +0200

    added undotree and other options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,18 @@ vim.wo.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
